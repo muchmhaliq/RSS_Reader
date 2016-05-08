7a7f46dbfa7507001792115caf7d8ff97b5be544
Ritesh <blackkukreja@gmail.com>
2016-05-08 09:10:18 +0530

    Added Error Handling

--- source/feed.php
+++ source/feed.php
@@ -1,12 +1,14 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-libxml_use_internal_errors(true);
+/**
+ * Validate that the URL is present
+ */
+if(!$_GET['url']) die('{}');
 
-if(!$_GET['url']) {
-	echo "{}";
-	exit();
-}
+/**
+ * Validate the given URL is a valid URL
+ */
+if(!filter_var($_GET['url'], FILTER_VALIDATE_URL) !== false) die('{}');
+
 
 $fileContents= file_get_contents($_GET["url"]);
 
@@ -19,37 +21,20 @@ $fileContents = stripslashes($fileContents);
 $simpleXml = simplexml_load_string($fileContents, 'SimpleXMLElement', LIBXML_NOCDATA);
 
 if ($simpleXml === false) {
-    echo "Failed loading XML\n";
+    $response = array();
+    $response['success'] = false;
+    $response['errors'] = array();
+
     foreach(libxml_get_errors() as $error) {
-        echo "\t", $error->message;
+       $response['errors'][] = $error->message;
     }
+
+    echo json_encode($response);
 } else {
 
 	$json = json_encode($simpleXml);
 
 	echo $json;
-}
-/*
-$curl = curl_init();
-curl_setopt_array($curl, Array(
-	CURLOPT_URL            => $_GET['url'],
-	CURLOPT_RETURNTRANSFER => TRUE,
-	CURLOPT_ENCODING       => 'UTF-8'
-	));
-$data = curl_exec($curl);
-curl_close($curl);
-$xml = simplexml_load_string($data, 'SimpleXMLElement', LIBXML_NOCDATA);
-echo json_encode($xml);
-*/
-
-function getSSLPage($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_SSLVERSION,3); 
-    $result = curl_exec($ch);
-    curl_close($ch);
-    return $result;
 }
 
 ?>
